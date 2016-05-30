76a3c9d6b7aa71dc295f5f5ea6564455804889c4
sukhesh <sukhesh222@gmail.com>
2016-05-30 12:03:26 +0530

    Initial Commit

---
+++ account.js
@@ -0,0 +1,25 @@
+
+
+			function DropDown(el) {
+				this.dd = el;
+				this.initEvents();
+			}
+			DropDown.prototype = {
+				initEvents : function() {
+					var obj = this;
+ss
+					});	
+				}
+			}
+
+			$(function() {
+
+				var dd = new DropDown( $('#dd') );
+
+				$(document).click(function() {
+					// all dropdowns
+					$('.wrapper-dropdown-5').removeClass('active');
+				});
+
+			});
+
